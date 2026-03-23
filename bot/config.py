2bbabbd0c72a84768a242cfc658d35374f5875b7
andrew1906fomenko-hue <andrew1906fomenko@gmail.com>
2026-03-23 20:41:19 +0300

    add render url

--- bot/config.py
+++ bot/config.py
@@ -1,3 +1,3 @@
-TOKEN = "YOUR_BOT_TOKEN"
-ADMIN_ID = 123456789
-WEB_APP_URL = "https://your-site.onrender.com"
+TOKEN = "8712751779:AAGndMBokm4oM_HtwJZirL1HrdvAJQcGd1M"
+ADMIN_ID = 5249205025
+WEB_APP_URL = "https://salon-2qs9.onrender.com"
