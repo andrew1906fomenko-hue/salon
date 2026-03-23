090c6693f4944d663675d807359ba6e4c112c138
andrew1906fomenko-hue <andrew1906fomenko@gmail.com>
2026-03-23 22:07:28 +0300

    fix token env

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,9 @@
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
+
+if not TOKEN:
+    raise ValueError("BOT_TOKEN is not set!")
+
 ADMIN_ID = 5249205025
 WEB_APP_URL = "https://salon-2qs9.onrender.com/"
